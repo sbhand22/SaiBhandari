ece5d0fffddcb46787cb748fcdf8d84a58b3ba98
sbhand22 <sbhand22@asu.edu>
2023-09-20 22:50:43 -0700

    Updates

---
+++ precache-manifest.7e9394076f188506366af37b93e84c81.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "307ab2ac31ec10736b5f9a9222a96bfb",
+    "url": "/SaiBhandari/index.html"
+  },
+  {
+    "revision": "d724d0245c1c8cb3d7c8",
+    "url": "/SaiBhandari/static/css/2.766850b1.chunk.css"
+  },
+  {
+    "revision": "1913722a55b0ea5fb8a1",
+    "url": "/SaiBhandari/static/css/main.97deef7a.chunk.css"
+  },
+  {
+    "revision": "d724d0245c1c8cb3d7c8",
+    "url": "/SaiBhandari/static/js/2.05cfd20a.chunk.js"
+  },
+  {
+    "revision": "9ab008f46842d036bb7d6edad80f72a5",
+    "url": "/SaiBhandari/static/js/2.05cfd20a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1913722a55b0ea5fb8a1",
+    "url": "/SaiBhandari/static/js/main.473f2330.chunk.js"
+  },
+  {
+    "revision": "7f159ed8140f80c56fad",
+    "url": "/SaiBhandari/static/js/runtime-main.f02acd13.js"
+  },
+  {
+    "revision": "39fbdec656bdbdaa1f8b7e8c8fec0df0",
+    "url": "/SaiBhandari/static/media/Kira-img.39fbdec6.png"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/SaiBhandari/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/SaiBhandari/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/SaiBhandari/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/SaiBhandari/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/SaiBhandari/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "28950c5f5027bc723e3dfcba1ec08ee7",
+    "url": "/SaiBhandari/static/media/cancer-info-ss.28950c5f.png"
+  },
+  {
+    "revision": "31febaa8de587cb1cf92d45222542069",
+    "url": "/SaiBhandari/static/media/disease-info-ss.31febaa8.png"
+  },
+  {
+    "revision": "60b45be1f22f03635ff3b8c8e08b741e",
+    "url": "/SaiBhandari/static/media/find-derma-ss.60b45be1.png"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/SaiBhandari/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0453a4f602b7dc2ccec968518d39081a",
+    "url": "/SaiBhandari/static/media/get-tested-ss.0453a4f6.png"
+  },
+  {
+    "revision": "b935dd22080193026c2575dea957d107",
+    "url": "/SaiBhandari/static/media/homepage-ss.b935dd22.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/SaiBhandari/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/SaiBhandari/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/SaiBhandari/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/SaiBhandari/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/SaiBhandari/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "fdee2d272c9d6d2265c622983a5c236b",
+    "url": "/SaiBhandari/static/media/kira-blog-ss.fdee2d27.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/SaiBhandari/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/SaiBhandari/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/SaiBhandari/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/SaiBhandari/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/SaiBhandari/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0764e3f5953727a8268acb27cf609a5c.js"
+  "/SaiBhandari/precache-manifest.7e9394076f188506366af37b93e84c81.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SaiBhandari/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
